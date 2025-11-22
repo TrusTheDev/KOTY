304179d4c3762bb2a307350906c2460a040413ea
Trusty <ivanbordeira2015@hotmail.com>
2025-11-22 18:50:45 -0300

    first version

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Homepage } from './homepage/homepage';
+import { Categories } from './categories/categories';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Homepage, Categories],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
+
 export class App {
   protected readonly title = signal('KanuterOfTheYear');
 }
